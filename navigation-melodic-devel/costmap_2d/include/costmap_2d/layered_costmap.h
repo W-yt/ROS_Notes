2cdf7252de9b776a475604e21cd755ad7e58bac7
W-yt <lalalu9514@gmail.com>
2021-12-05 23:27:47 +0800

    learn the costmap2d to Costmap2D::convexFillCells function

--- navigation-melodic-devel/costmap_2d/include/costmap_2d/layered_costmap.h
+++ navigation-melodic-devel/costmap_2d/include/costmap_2d/layered_costmap.h
@@ -46,131 +46,131 @@
 
 namespace costmap_2d
 {
-class Layer;
-
-/**
- * @class LayeredCostmap
- * @brief Instantiates different layer plugins and aggregates them into one score
- */
-class LayeredCostmap
-{
-public:
-  /**
-   * @brief  Constructor for a costmap
-   */
-  LayeredCostmap(std::string global_frame, bool rolling_window, bool track_unknown);
+  class Layer;
 
   /**
-   * @brief  Destructor
+   * @class LayeredCostmap
+   * @brief Instantiates different layer plugins and aggregates them into one score
    */
-  ~LayeredCostmap();
-
-  /**
-   * @brief  Update the underlying costmap with new data.
-   * If you want to update the map outside of the update loop that runs, you can call this.
-   */
-  void updateMap(double robot_x, double robot_y, double robot_yaw);
-
-  std::string getGlobalFrameID() const
+  class LayeredCostmap
   {
-    return global_frame_;
-  }
+    public:
+      /**
+       * @brief  Constructor for a costmap
+       */
+      LayeredCostmap(std::string global_frame, bool rolling_window, bool track_unknown);
 
-  void resizeMap(unsigned int size_x, unsigned int size_y, double resolution, double origin_x, double origin_y,
-                 bool size_locked = false);
+      /**
+       * @brief  Destructor
+       */
+      ~LayeredCostmap();
 
-  void getUpdatedBounds(double& minx, double& miny, double& maxx, double& maxy)
-  {
-    minx = minx_;
-    miny = miny_;
-    maxx = maxx_;
-    maxy = maxy_;
-  }
+      /**
+       * @brief  Update the underlying costmap with new data.
+       * If you want to update the map outside of the update loop that runs, you can call this.
+       */
+      void updateMap(double robot_x, double robot_y, double robot_yaw);
 
-  bool isCurrent();
+      std::string getGlobalFrameID() const
+      {
+        return global_frame_;
+      }
 
-  Costmap2D* getCostmap()
-  {
-    return &costmap_;
-  }
+      void resizeMap(unsigned int size_x, unsigned int size_y, double resolution, double origin_x, double origin_y,
+                    bool size_locked = false);
 
-  bool isRolling()
-  {
-    return rolling_window_;
-  }
+      void getUpdatedBounds(double& minx, double& miny, double& maxx, double& maxy)
+      {
+        minx = minx_;
+        miny = miny_;
+        maxx = maxx_;
+        maxy = maxy_;
+      }
 
-  bool isTrackingUnknown()
-  {
-    return costmap_.getDefaultValue() == costmap_2d::NO_INFORMATION;
-  }
+      bool isCurrent();
 
-  std::vector<boost::shared_ptr<Layer> >* getPlugins()
-  {
-    return &plugins_;
-  }
+      Costmap2D* getCostmap()
+      {
+        return &costmap_;
+      }
 
-  void addPlugin(boost::shared_ptr<Layer> plugin)
-  {
-    plugins_.push_back(plugin);
-  }
+      bool isRolling()
+      {
+        return rolling_window_;
+      }
 
-  bool isSizeLocked()
-  {
-    return size_locked_;
-  }
+      bool isTrackingUnknown()
+      {
+        return costmap_.getDefaultValue() == costmap_2d::NO_INFORMATION;
+      }
 
-  void getBounds(unsigned int* x0, unsigned int* xn, unsigned int* y0, unsigned int* yn)
-  {
-    *x0 = bx0_;
-    *xn = bxn_;
-    *y0 = by0_;
-    *yn = byn_;
-  }
+      std::vector<boost::shared_ptr<Layer> >* getPlugins()
+      {
+        return &plugins_;
+      }
 
-  bool isInitialized()
-  {
-      return initialized_;
-  }
+      void addPlugin(boost::shared_ptr<Layer> plugin)
+      {
+        plugins_.push_back(plugin);
+      }
 
-  /** @brief Updates the stored footprint, updates the circumscribed
-   * and inscribed radii, and calls onFootprintChanged() in all
-   * layers. */
-  void setFootprint(const std::vector<geometry_msgs::Point>& footprint_spec);
+      bool isSizeLocked()
+      {
+        return size_locked_;
+      }
 
-  /** @brief Returns the latest footprint stored with setFootprint(). */
-  const std::vector<geometry_msgs::Point>& getFootprint() { return footprint_; }
+      void getBounds(unsigned int* x0, unsigned int* xn, unsigned int* y0, unsigned int* yn)
+      {
+        *x0 = bx0_;
+        *xn = bxn_;
+        *y0 = by0_;
+        *yn = byn_;
+      }
 
-  /** @brief The radius of a circle centered at the origin of the
-   * robot which just surrounds all points on the robot's
-   * footprint.
-   *
-   * This is updated by setFootprint(). */
-  double getCircumscribedRadius() { return circumscribed_radius_; }
+      bool isInitialized()
+      {
+          return initialized_;
+      }
 
-  /** @brief The radius of a circle centered at the origin of the
-   * robot which is just within all points and edges of the robot's
-   * footprint.
-   *
-   * This is updated by setFootprint(). */
-  double getInscribedRadius() { return inscribed_radius_; }
+      /** @brief Updates the stored footprint, updates the circumscribed
+       * and inscribed radii, and calls onFootprintChanged() in all
+       * layers. */
+      void setFootprint(const std::vector<geometry_msgs::Point>& footprint_spec);
 
-private:
-  Costmap2D costmap_;
-  std::string global_frame_;
+      /** @brief Returns the latest footprint stored with setFootprint(). */
+      const std::vector<geometry_msgs::Point>& getFootprint() { return footprint_; }
 
-  bool rolling_window_;  /// < @brief Whether or not the costmap should roll with the robot
+      /** @brief The radius of a circle centered at the origin of the
+       * robot which just surrounds all points on the robot's
+       * footprint.
+       *
+       * This is updated by setFootprint(). */
+      double getCircumscribedRadius() { return circumscribed_radius_; }
 
-  bool current_;
-  double minx_, miny_, maxx_, maxy_;
-  unsigned int bx0_, bxn_, by0_, byn_;
+      /** @brief The radius of a circle centered at the origin of the
+       * robot which is just within all points and edges of the robot's
+       * footprint.
+       *
+       * This is updated by setFootprint(). */
+      double getInscribedRadius() { return inscribed_radius_; }
 
-  std::vector<boost::shared_ptr<Layer> > plugins_;
+    private:
+      Costmap2D costmap_;
+      std::string global_frame_;
 
-  bool initialized_;
-  bool size_locked_;
-  double circumscribed_radius_, inscribed_radius_;
-  std::vector<geometry_msgs::Point> footprint_;
-};
+      bool rolling_window_;  /// < @brief Whether or not the costmap should roll with the robot
+
+      bool current_;
+      double minx_, miny_, maxx_, maxy_;
+      unsigned int bx0_, bxn_, by0_, byn_;
+
+      std::vector<boost::shared_ptr<Layer> > plugins_;
+
+      bool initialized_;
+      bool size_locked_;
+      double circumscribed_radius_, inscribed_radius_;
+      std::vector<geometry_msgs::Point> footprint_;
+  };
 
 }  // namespace costmap_2d
 
